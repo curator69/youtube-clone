7cf1924524510d977199fa119eb20c5629fe683d
curator69 <75662004+curator69@users.noreply.github.com>
2022-09-17 01:26:37 +0530

    Updated src folder

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from "@mui/material";
+import { SearchIcon } from "@mui/icons-material";
+
+const SearchBar = () => {
+  return (
+    <Paper
+      component="form"
+      onSubmit={() => {}}
+      sx={{ borderRadius: 20 }}
+    ></Paper>
+  );
+};
+
+export default SearchBar;
