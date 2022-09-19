254467118dfe2a47487dbbcbcc4bed1bb588cdd9
curator69 <75662004+curator69@users.noreply.github.com>
2022-09-19 23:24:21 +0530

    completed search functionality

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -13,7 +13,7 @@ const SearchFeed = () => {
     fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
       setVideos(data.items)
     );
-  }, []);
+  }, [searchTerm]);
 
   return (
     <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
@@ -31,8 +31,9 @@ const SearchFeed = () => {
             color: "#F31503",
           }}
         >
-          videos
-        </span>
+          {searchTerm}
+        </span>{" "}
+        videos
       </Typography>
 
       <Videos videos={videos} />
